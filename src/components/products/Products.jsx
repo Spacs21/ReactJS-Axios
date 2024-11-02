d4495680477978dae75ddb8326f6f0522611062e
Spacs21 <behaksr123@gmail.com>
2024-11-02 22:39:42 +0500

    optimised code and added custom hooks

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,16 +1,20 @@
-import axios from 'axios'
+import axios from '../../api'
 import React, { useEffect, useState } from 'react'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import "./Products.scss"
 import { CiStar } from "react-icons/ci";
 import { FaStar } from "react-icons/fa";
+import { useFetch } from '../../hooks/useFetch';
 
-const BASE_URL = "https://dummyjson.com"
+
 
 const Products = () => {
-    const [products, setProducts] = useState(null) 
     const [limit, setLimit] = useState(6)
+    const [offset, setOffset] = useState(6)
+    const [category, setCategory] = useState("/")
+    const {data, loading, error} = useFetch(`/products${category}`, {limit: limit}, [limit, category])
+    const {data: categories} = useFetch("/products/category-list")
 
     useEffect(() => {
         AOS.init({
@@ -20,20 +24,24 @@ const Products = () => {
     }, []);
 
     const handleLimit = () => {
-        setLimit(prevLimit => prevLimit + 6)
+        setLimit(prevLimit => prevLimit + offset)
     }
 
-    useEffect(()=> {
-        axios
-            .get(`${BASE_URL}/products?limit=${limit}`)
-            .then(res => {
-                setProducts(res.data.products)
-            })
-            .catch(err => console.log(err))
-            .finally() 
-    }, [limit])
-    console.log(products)
-    const productItems = products?.map((product)=>(
+    // useEffect(()=> {
+    //     axios
+    //         .get(`/products?limit=${limit}`)
+    //         .then(res => {
+    //             setProducts(res.data.products)
+    //         })
+    //         .catch(err => console.log(err))
+    //         .finally() 
+    // }, [limit])
+
+    // useEffect(()=>{
+    //     axios.get(`/products/category-list`)
+    // })
+
+    const productItems = data?.products?.map((product)=>(
         <div className="carts__item" key={product.id} data-aos="fade-up">
             <img src={product.images[0]}/>
             <div className="carts__item__title">
@@ -49,7 +57,12 @@ const Products = () => {
             </div>
         </div>
     ))
-
+    const cateogryItems = categories?.map((item)=>(
+        <li className='categories__item' key={item} onClick={()=> setCategory(`/category/${item}`)}>
+            {item}
+        </li>
+    ))
+    // console.log(category);
   return (
 
     <main>
@@ -58,6 +71,9 @@ const Products = () => {
                 <p className='tag'>M E N U</p>
                 <h2>Food Full of treaty Love</h2>
                 <p className='title__descp'>There are many things are needed to start the Fast Food Business. You need not only Just Food Stalls with Persons but also specialized equipment, Skills to manage Customers, </p>
+            </div>
+            <div className="categories">
+                {cateogryItems}
             </div>
             <div className="products">
                 <div className="carts">
